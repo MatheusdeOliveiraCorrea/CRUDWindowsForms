1cb1f1fc57246c663460e8a946c0c06e1bf4f4a7
MatheusdeOliveiraCorrea <matheus82pessoal@protonmail.com>
2022-08-03 14:29:34 -0300

    tentando implementar criptografia senha

--- WinFormsApp1/Program.cs
+++ WinFormsApp1/Program.cs
@@ -1,9 +1,8 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Security.Cryptography;
+using System.Text;
 using System.Windows.Forms;
-
+using WinFormsApp1.Servicos;
 namespace WinFormsApp1
 {
     internal static class Program
@@ -14,11 +13,28 @@ namespace WinFormsApp1
         [STAThread]
         static void Main()
         {
+            var senha = "Ola, meu nome é Matheus";
+            var conversor = new UnicodeEncoding();
 
-            Application.SetHighDpiMode(HighDpiMode.SystemAware);
+
+            //var textoPlano = new byte[2048];
+            //var textoCifrado = new byte[2048];
+            var textoDecifrado = new byte[2048];
+
+            /* textoPlano = conversor.GetBytes(senha);
+             textoCifrado = EncriptografarSenha.RSACifra(textoPlano, rsa.ExportParameters(false), false);*/
+
+            string aposSerCifradoMetodo = EncriptografarSenha.CifrarString(senha);
+            byte[] cifrado = conversor.GetBytes(aposSerCifradoMetodo);
+
+            textoDecifrado = EncriptografarSenha.RSADecifra(cifrado, EncriptografarSenha.rsa.ExportParameters(true), false);
+
+            string aposSerDecifrado = conversor.GetString(textoDecifrado);
+
+            /*Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new TelaPrincipal());
+            Application.Run(new TelaPrincipal());*/
 
         }
     }
---
+++ WinFormsApp1/Servicos/EncriptografarSenha.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace WinFormsApp1.Servicos
+{
+
+    public class EncriptografarSenha
+    {
+
+        public static RSACryptoServiceProvider rsa = new RSACryptoServiceProvider();
+
+        static public string CifrarString(string senha)
+        {
+            var conversor = new UnicodeEncoding();
+            byte[] textoPlano = conversor.GetBytes(senha);
+            var re = RSACifra(textoPlano, rsa.ExportParameters(false), false);
+
+            return conversor.GetString(re);
+        }
+
+        static public byte[] RSACifra(byte[] byteCifrado, RSAParameters RSAInfo, bool isOAEP)
+        {
+            try
+            {
+                byte[] DadosCifrados;
+                using (RSACryptoServiceProvider RSA = new RSACryptoServiceProvider())
+                {
+                    RSA.ImportParameters(RSAInfo);
+                    DadosCifrados = RSA.Encrypt(byteCifrado, isOAEP);
+                }
+                return DadosCifrados;
+            }
+            catch (CryptographicException e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+
+        static public byte[] RSADecifra(byte[] textoCifrado, RSAParameters RSAInfo, bool isOAEP)
+        {
+            try
+            {
+                byte[] DadosDecifrados;
+
+                using (RSACryptoServiceProvider RSA = new RSACryptoServiceProvider())
+                {
+                    RSA.ImportParameters(RSAInfo);
+                    DadosDecifrados = RSA.Decrypt(textoCifrado, isOAEP);
+                }
+                return DadosDecifrados;
+            }
+            catch (CryptographicException e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+    }
+}
+
+
+
+/*using System;
+using System.Security.Cryptography;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WinFormsApp1.Servicos
+{
+    public class EncriptografarSenha
+    {
+
+        public EncriptografarSenha(string senha)
+        {
+            try
+            {
+                var conv = new UnicodeEncoding();
+
+                //Create byte arrays to hold original, encrypted, and decrypted data.
+                byte[] aCriptografar = conv.GetBytes(senha);
+                byte[] encriptografado;
+                byte[] desencriptografado;
+
+                using (RSACryptoServiceProvider RSA = new RSACryptoServiceProvider())
+                {
+                    encriptografado = Cifrar(aCriptografar, PegarChavePublica(), false);
+                    desencriptografado = Decifrar(encriptografado, RSA.ExportParameters(true), false);
+                }
+
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+
+        public byte[] Cifrar(byte[] aCriptografar, RSAParameters RSAInformacaoChave, bool FazerOAEPPadding)
+        {
+            try
+            {
+                byte[] encriptografado;
+                using (RSACryptoServiceProvider RSA = new RSACryptoServiceProvider())
+                {
+                    RSA.ImportParameters(RSAInformacaoChave);
+                    encriptografado = RSA.Decrypt(aCriptografar, FazerOAEPPadding);
+                }
+                return encriptografado;
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+                return null;
+            }
+        }
+
+        public byte[] Decifrar(byte[] aDesencriptografar, RSAParameters RSAInformacaoChave, bool FazerOAEPPadding)
+        {
+            try
+            {
+                byte[] encriptografado;
+
+                using (RSACryptoServiceProvider RSA = new RSACryptoServiceProvider())
+                {
+
+                    RSA.ImportParameters(RSAInformacaoChave);
+
+                    encriptografado = RSA.Decrypt(aDesencriptografar, FazerOAEPPadding);
+                }
+                return encriptografado;
+
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+                return null;
+            }
+        }
+
+        public RSAParameters PegarChavePublica()
+        {
+            using (RSACryptoServiceProvider RSA = new RSACryptoServiceProvider())
+            {
+                RSAParameters RSAParams = RSA.ExportParameters(false);
+                return RSAParams;
+            }
+        }
+    }
+}*/
