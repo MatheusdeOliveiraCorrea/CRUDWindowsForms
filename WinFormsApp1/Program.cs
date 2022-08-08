c02085cb8cefa3660ca181496cd72b439a7e7f00
MatheusdeOliveiraCorrea <matheus82pessoal@protonmail.com>
2022-08-08 09:21:00 -0300

    finalizado injecao de dependencia

--- WinFormsApp1/Program.cs
+++ WinFormsApp1/Program.cs
@@ -5,7 +5,6 @@ using Microsoft.Extensions.Hosting;
 using System;
 using System.Windows.Forms;
 namespace CrudWindowsForms.InterfaceDoUsuario
-
 {
     internal static class Program
     {
@@ -20,15 +19,13 @@ namespace CrudWindowsForms.InterfaceDoUsuario
             Application.SetCompatibleTextRenderingDefault(false);
 
             var host = CreateHostBuilder().Build();
-            var formTelaPrincipal = host.Services.GetRequiredService<TelaPrincipal>();
-            Application.Run(formTelaPrincipal);
-
+            var usuarioRepositorio = host.Services.GetService<IUsuarioRepositorio>();
+            Application.Run(new TelaPrincipal(usuarioRepositorio));
         }
 
         static IHostBuilder CreateHostBuilder() =>
         Host.CreateDefaultBuilder()
             .ConfigureServices((context, services) =>
-                services.AddTransient<IUsuarioRepositorio, BDUsuario>()
-                        .AddTransient<TelaPrincipal>());
+                services.AddScoped<IUsuarioRepositorio, BDUsuario>());
     }
 }
